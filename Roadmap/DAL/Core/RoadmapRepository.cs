73d8f3e6f682a6f40a7d560a9ae9c5177a30d08d
Vitalii <Vitalii@WINCTRL-PUM6NBD>
2016-11-01 22:21:27 +0200

    UnitOfWork.cs + RoadmapRepository.cs was created

---
+++ Roadmap/DAL/Core/RoadmapRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Ajax.Utilities;
+using System.Data.Entity;
+using System.Linq.Expressions;
+
+
+namespace Roadmap.DAL.Core
+{
+    public class RoadmapRepository
+    {
+        protected DbContext context;
+        
+        public RoadmapRepository(DbContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<T> Get<T>() where T : class
+        {
+            return context.Set<T>();
+        }
+
+        public T Get<T>(Expression<Func<T, bool>> condition) where T : class
+        {
+            return this.context.Set<T>().FirstOrDefault(condition);
+        }
+
+        public void Add<T>(T item) where T : class
+        {
+            this.context.Set<T>().Add(item);
+        }
+
+        public void Update<T>(T item) where T : class
+        {
+            this.context.Entry(item).State = EntityState.Modified;
+        }
+
+        public void Delete<T>(T item) where T : class
+        {
+            this.context.Set<T>().Remove(item);
+        }
+    }
+}
---
+++ Roadmap/DAL/Core/UnitOfWork.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Roadmap.DAL;
+
+namespace Roadmap.DAL.Core
+{
+    public class UnitOfWork : IDisposable
+    {
+        private RoadmapContext db;
+        private RoadmapRepository roadmapRepository;
+        private bool disposed = false;
+
+        public UnitOfWork()
+        {
+            db = new RoadmapContext();
+        }
+
+        public RoadmapRepository RoadmapRepository
+        {
+            get
+            {
+                if (roadmapRepository == null)
+                {
+                    roadmapRepository = new RoadmapRepository(db);
+                }                    
+
+                return roadmapRepository;
+            }
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Save()
+        {
+            try
+            {
+                db.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                LogError(ex);
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void LogError(Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex.Message);
+
+            if (ex.InnerException != null)
+            {
+                LogError(ex.InnerException);
+            }
+        }
+    }
+}
